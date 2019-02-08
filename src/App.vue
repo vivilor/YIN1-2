4a83c4f64971691c0177ba64e008931e3d9cdbdf
Viktor <viktor.livantsov@outlook.com>
2019-02-08 23:11:52 +0300

    #time 15m #comment done with basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex">
+    <div class="workfield flex flex__column flex__fill">
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @import "assets/styles/font";
+@import "assets/styles/common";
+
 #app {
+  width: $app-width;
+  height: $app-height;
+  background: white;
+
+  & > .workfield {
+    position: relative;
+    margin: $top-margin $side-margin $bottom-margin;
+  }
 }
 </style>
