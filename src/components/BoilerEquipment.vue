8ec33ae5e8088049f41aa867c066d07bfe247865
Viktor <viktor.livantsov@outlook.com>
2019-02-09 14:49:35 +0300

    #time 12m #comment add component for boiler equipment

---
+++ src/components/BoilerEquipment.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <img src="../assets/img/boiler-equipment.png" alt="boiler-equipment">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BoilerEquipment'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
