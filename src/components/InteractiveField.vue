ee5503ba3538eedb789d41cc6999317751b3ee90
Viktor <viktor.livantsov@outlook.com>
2019-02-09 17:04:33 +0300

    #time 13m #comment add interactive field

---
+++ src/components/InteractiveField.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'InteractiveField',
+  render (h) {
+    return h(
+      'section',
+      this.$slots.default
+    )
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+  flex: initial;
+  width: inherit;
+}
+</style>
